de3d4af2bdf6f29eeff335e51684209c958022d7
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-09-14 10:08:39 +0530

    Vite App

---
+++ React Js/react-js-concepts/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ClassBasedComponent from './components/class-based-component'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1> React JS Concepts 2024 </h1>
+        <ClassBasedComponent/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ React Js/react-js-concepts/src/components/class-based-component.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react"
+
+class ClassBasedComponent extends Component {
+    render(){
+        return(
+        <div>
+        <h3> Class based component </h3>
+        </div>
+    );
+}
+}
+export default ClassBasedComponent;
