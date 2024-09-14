5a6b47dfdfc606ca092a7470518155851c623073
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-09-14 10:31:11 +0530

    components

--- React Js/react-js-concepts/src/App.jsx
+++ React Js/react-js-concepts/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ClassBasedComponent from './components/class-based-component'
+import FunctionalComponent from './components/functional-component'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <div>
         <h1> React JS Concepts 2024 </h1>
         <ClassBasedComponent/>
+        <FunctionalComponent/>
       </div>
     </>
   )
