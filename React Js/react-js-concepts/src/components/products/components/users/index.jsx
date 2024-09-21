24cb737c9d371ea24c27469194c30626c37323eb
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-09-21 11:15:12 +0530

    fetchAllUsersData

--- React Js/react-js-concepts/src/components/products/components/users/index.jsx
+++ React Js/react-js-concepts/src/components/products/components/users/index.jsx
@@ -2,13 +2,16 @@ import { useEffect, useState } from "react"
 
 export default function Users(){
     const [usersList, setUsersList] = useState([]);
+    const [pending, setPending] = useState(false);
 
     const fetchAllUsers = async () => {
         try{
+            setPending(false);
             const apiResponse = await fetch('https://dummyjson.com/users');
             const result = await apiResponse.json();
             if(result?.users){
-                setUsersList(result?.users)
+                setUsersList(result?.users);
+                setPending(false);
             }else{
                 setUsersList([]);
             }
@@ -17,11 +20,31 @@ export default function Users(){
             console.log(error);
         }
     }
-    useEffect(() => {
-        fetchAllUsers();
-    }, []);
+
+    // function handleFetchListOfUsers(){
+    //     fetchAllUsers();
+    // }
+
+    // useEffect(() => {
+    //     fetchAllUsers();
+    // }, []);
+
+    console.log(usersList);
+
+    if(pending) return <h1> Fetching users! Please wait </h1>
+
     return <div>
         <h1> All Users Lists </h1>
+        <button onClick={fetchAllUsers}> Fetch Users List </button>
+        <ul>
+            {
+                usersList && usersList.length > 0 ? 
+                usersList.map(usersItem => <li key={usersItem?.id}>
+                    <p>{usersItem?.firstName} {usersItem?.lastName} </p>
+
+                </li>) : <h1> No users found! Please try again. </h1>
+            }
+        </ul>
     </div>
 }
 
